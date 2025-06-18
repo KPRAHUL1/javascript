99ba9af59b05010e34b81bcdf49eff8345020f6c
Rahul K <rahulsachin07314@gmail.com>
2025-06-18 22:43:25 +0530

    javascript problems

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 // fruit();
 //////////////////////////////////////////////////
 // for (let i = 0; i < 3; i++) {
-//     setTimeout(()=> console.log(i),1);
+//     setTimeout(()=> console.log(i),1); 
     
 // }
 // for (var i = 0; i < 3; i++) {
