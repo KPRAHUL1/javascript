ce395c0ce98dcd034561b4be1dce18f439e611ed
Rahul K <rahulsachin07314@gmail.com>
2025-07-09 11:33:14 +0530

    create api and integrate api for editRoleById

--- index.js
+++ index.js
@@ -203,24 +203,24 @@
 // let middle =start+(end-start)/2 ;
 // console.log(middle);
 
-var searchInsert = function(nums, target) {
-    let start = 0;
-    let end = nums.length-1;
+// var searchInsert = function(nums, target) {
+//     let start = 0;
+//     let end = nums.length-1;
     
-  while (start<=end) {
-    let mid = Math.floor((start + end)/2);
-   if(target == nums[mid]){
-       return mid;
-   }
-   else if(nums[mid]<target){
-    start = mid +1;
-   }
-   else{
-    end = mid-1;
-   }
-  }
-  return start;
-};
-let nums = [1,3,5,6]
-let target = 6;
-console.log(searchInsert(nums,target));
+//   while (start<=end) {
+//     let mid = Math.floor((start + end)/2);
+//    if(target == nums[mid]){
+//        return mid;
+//    }
+//    else if(nums[mid]<target){
+//     start = mid +1;
+//    }
+//    else{
+//     end = mid-1;
+//    }
+//   }
+//   return start;
+// };
+// let nums = [1,3,5,6]
+// let target = 6;
+// console.log(searchInsert(nums,target));
